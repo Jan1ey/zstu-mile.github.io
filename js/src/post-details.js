1b51318b172b7337629285625f8ad5747aba341c
zstu-mile <le03560@163.com>
2019-05-16 20:13:49 +0800

    Site updated: 2019-05-16 20:13:48

--- js/src/post-details.js
+++ js/src/post-details.js
@@ -1,5 +1,12 @@
 /* global NexT: true */
-
+$(document).ready(function(){
+  $(document).on('click', '.fold_hider', function(){
+      $('>.fold', this.parentNode).slideToggle();
+      $('>:first', this).toggleClass('open');
+  });
+  //默认情况下展开
+  $("div.fold").css("display","open");
+});
 $(document).ready(function () {
 
   initScrollSpy();
